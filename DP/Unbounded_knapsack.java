513c631f8e347b3f815317dd062bfb4cd9efbef7
akshansh496 <guptaakshansh1@gmail.com>
2026-02-11 22:43:55 +0530

    practice

---
+++ DP/Unbounded_knapsack.java
@@ -0,0 +1,42 @@
+package DP;
+
+import java.util.Arrays;
+
+public class Unbounded_knapsack {
+    public static int unboundedknapsackTab(int val[],int wt[],int W){
+        int n=val.length;
+        int dp[][]=new int[n+1][W+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<dp[0].length;i++){
+            dp[0][i]=0;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int v=val[i-1];
+                int w=wt[i-1];
+                if(w<=j){
+                    int incProfit=v+dp[i][j-w];
+                    int excProfit=dp[i-1][j];
+                    dp[i][j]=Math.max(incProfit,excProfit);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+    public static void main(String[] args) {
+        int val[]={15,14,10,45,30};
+        int wt[]={2,5,1,3,4};
+        int W=7;
+        int memory[][]=new int[W+1][val.length+1];
+        for (int i = 0; i <= W; i++) {
+            Arrays.fill(memory[i], -1);
+        }
+        System.out.println(unboundedknapsackTab(val, wt, W));
+    }   
+}
